064cf14b7bb9f926b1d5817d258d5a735585f1f9
Nicholas Engleman <nicholas@englemaninvest.com>
2018-05-14 02:46:26 -0400

    bug fix for heroku deployment

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/api/GetAllProducts', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/api/user_boards', (req, res) => {
