8d988e1b3148a333085ecfbcbd8a88ca25972b4d
Nicholas Engleman <nicholas@englemaninvest.com>
2018-05-14 00:11:03 -0400

    misc bug fixes

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const config = {
 	production: {
 		SECRET: process.env.SECRET,
-		DATABASE: process.env.MONGODB_URI
+		DATABASE: 'mongodb://engleman:11july2017@ds119650.mlab.com:19650/pinterest-7512'
 	},
 	default: {
 		SECRET: 'supersecretpassword',
