432272c6d829943f67ac7c4145268998f0774871
Nicholas Engleman <nicholas@englemaninvest.com>
2018-05-20 15:16:19 -0400

    bug fixes for mobile register component

--- client/src/components/LoginRegisterModal/LoginRegisterModal.js
+++ client/src/components/LoginRegisterModal/LoginRegisterModal.js
@@ -120,10 +120,10 @@ class LoginRegisterModal extends React.Component {
 	};
 
 	checkForMatchingPasswords = () => {
-		if(this.state.signUpPassword !== this.state.signUpPasswordConfirm) {
+		if (this.state.signUpPassword !== this.state.signUpPasswordConfirm) {
 			this.setState({statusCreateAccount: "passwords do not match"});
 		} else {
-			this.setState({statusCreateAccount: "" });
+			this.setState({statusCreateAccount: ""});
 		}
 	};
 
@@ -137,84 +137,86 @@ class LoginRegisterModal extends React.Component {
 				contentLabel="Login/Register Modal"
 				bodyOpenClassName="ReactModal__Body--open"
 			>
-				<header className="loginRegisterModal__header">
-					<Heading size="xs" color="red">
-						Welcome to <br/> ABC Store
-					</Heading>
-					<div className="registerIntro">
-						<Text size="md" align="center" color="gray">
-							Find and comment on <br/>products you like.
-						</Text>
-						<br/>
-						<Text size="md" align="center" color="gray">
-							Save to boards the products <br/>you like the most.
-						</Text>
+				<div className="registerContainer">
+					<header className="loginRegisterModal__header">
+						<Heading size="xs" color="red">
+							Welcome to <br/> ABC Store
+						</Heading>
+						<div className="registerIntro">
+							<Text size="md" align="center" color="gray">
+								Find and comment on <br/>products you like.
+							</Text>
+							<br/>
+							<Text size="md" align="center" color="gray">
+								Save to boards the products <br/>you like the most.
+							</Text>
+						</div>
+					</header>
+					<div className="loginRegisterModal__loginForm_error">
+						{this.state.statusSignIn}
 					</div>
-				</header>
-				<div className="loginRegisterModal__loginForm_error">
-					{this.state.statusSignIn}
-				</div>
-				<div className="loginRegisterModal__loginForm">
-					<input
-						name="email"
-						type="email"
-						ref={input => (this.logInEmail = input)}
-						onChange={this.onInputChange}
-						value={this.state.logInEmail}
-						placeholder="enter your email"
-						required
-					/>
-					<input
-						name="password"
-						type="password"
-						ref={input => (this.logInPassword = input)}
-						onChange={this.onInputChange}
-						value={this.state.logInPassword}
-						placeholder="enter your password"
-						required
-					/>
-					<Button text="Sign In" onClick={this.onSignIn} color="red"/>
-					<h3>or</h3>
-					<input
-						name="email"
-						type="email"
-						ref={input => (this.signUpEmail = input)}
-						onChange={this.onInputChange}
-						value={this.state.signUpEmail}
-						placeholder="enter your email"
-						required
-					/>
-					<input
-						name="name"
-						type="text"
-						ref={input => (this.signUpName = input)}
-						onChange={this.onInputChange}
-						value={this.state.signUpName}
-						placeholder="enter a first name"
-						required
-					/>
-					<input
-						name="password"
-						type="password"
-						ref={input => (this.signUpPassword = input)}
-						onChange={this.onInputChange}
-						value={this.state.signUpPassword}
-						placeholder="create a password"
-						required
-					/>
-					<input
-						name="passwordConfirm"
-						type="password"
-						ref={input => (this.signUpPasswordConfirm = input)}
-						onChange={this.onInputChange}
-						value={this.state.signUpPasswordConfirm}
-						placeholder="confirm your password"
-						required
-					/>
-					<Button text="Create Account" onClick={this.onSignUp} color="blue"/>
-				</div>
-				<div className="loginRegisterModal__loginForm_error">
-					{this.state.statusCreateAccount}
+					<div className="loginRegisterModal__loginForm">
+						<input
+							name="email"
+							type="email"
+							ref={input => (this.logInEmail = input)}
+							onChange={this.onInputChange}
+							value={this.state.logInEmail}
+							placeholder="enter your email"
+							required
+						/>
+						<input
+							name="password"
+							type="password"
+							ref={input => (this.logInPassword = input)}
+							onChange={this.onInputChange}
+							value={this.state.logInPassword}
+							placeholder="enter your password"
+							required
+						/>
+						<Button text="Sign In" onClick={this.onSignIn} color="red"/>
+						<h3>or</h3>
+						<input
+							name="email"
+							type="email"
+							ref={input => (this.signUpEmail = input)}
+							onChange={this.onInputChange}
+							value={this.state.signUpEmail}
+							placeholder="enter your email"
+							required
+						/>
+						<input
+							name="name"
+							type="text"
+							ref={input => (this.signUpName = input)}
+							onChange={this.onInputChange}
+							value={this.state.signUpName}
+							placeholder="enter a first name"
+							required
+						/>
+						<input
+							name="password"
+							type="password"
+							ref={input => (this.signUpPassword = input)}
+							onChange={this.onInputChange}
+							value={this.state.signUpPassword}
+							placeholder="create a password"
+							required
+						/>
+						<input
+							name="passwordConfirm"
+							type="password"
+							ref={input => (this.signUpPasswordConfirm = input)}
+							onChange={this.onInputChange}
+							value={this.state.signUpPasswordConfirm}
+							placeholder="confirm your password"
+							required
+						/>
+						<Button text="Create Account" onClick={this.onSignUp} color="blue"/>
+					</div>
+					<div className="loginRegisterModal__loginForm_error">
+						{this.state.statusCreateAccount}
+					</div>
 				</div>
 			</ReactModal>
 		);
